4fa1cafd271a81ad9202ec99277bd9c2712f37ed
harishkumargorinta <147605404+harishkumargorinta@users.noreply.github.com>
2024-07-05 00:36:00 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -37,7 +37,7 @@ async def save_group(bot, message):
             return
         buttons = [[
             InlineKeyboardButton('🤥 Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('🔔 Updates', url='https://t.me/LazyDeveloper')
+            InlineKeyboardButton('🔔 Updates', url='https://t.me/kumartvmovies')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
