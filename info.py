570d80c0a20aaf0ab3f49e8475a17113a738cdfb
harishkumargorinta <147605404+harishkumargorinta@users.noreply.github.com>
2024-07-06 07:00:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -45,7 +45,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002202065534'))
-LAZY_GROUP_LOGS = int(environ.get('LAZY_GROUP_LOGS', '-1004126757679'))
+LAZY_GROUP_LOGS = int(environ.get('LAZY_GROUP_LOGS', '-1002202065534'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'OGSUPPORTCHAT')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
@@ -68,7 +68,7 @@ LAZY_MODE = bool(environ.get("LAZY_MODE"))
 #Add user id of the user in this field those who you want to be Authentic user for file renaming features
 lazy_renamers = [int(lazrenamers) if id_pattern.search(lazrenamers) else lazrenamers for lazrenamers in environ.get('LAZY_RENAMERS', '').split()]
 LAZY_RENAMERS = (lazy_renamers + ADMINS) if lazy_renamers else []
-REQ_CHANNEL = int(environ.get('REQ_CHANNEL'))
+REQ_CHANNEL = int(environ.get('REQ_CHANNEL','-1004289157108'))
 
 #ai
 # OPENAI_API = environ.get("OPENAI_API","")
